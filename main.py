2b2cb6024ab911a15b9b68272e40ae59edbd8ae8
Igor <nekto.korobko@gmail.com>
2023-04-02 00:12:37 +0300

    download books with titles.

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
 import os
 
 import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+from requests import HTTPError
 
-directory = 'books'
-if not os.path.exists(directory):
-    os.makedirs(directory, exist_ok=True)
 
-url = 'https://tululu.org/txt.php?id='
-for id in range(1, 11):
-    response = requests.get(f'{url}{id}')
+def check_for_redirect(response):
+    if response.history:
+        raise HTTPError
+
+
+def download_txt(book_id, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    if not os.path.exists(folder):
+        os.makedirs(folder, exist_ok=True)
+    filepath = os.path.join(folder, sanitize_filename(filename))
+    url = f'https://tululu.org/txt.php?id={book_id}'
+    response = requests.get(url)
     response.raise_for_status()
-    filename = f'{id}.txt'
-    with open(f'{directory}/{filename}', 'w') as file:
-        file.write(response.text)
+    book = response.text
+    filename = f'{filepath}.txt'
+    with open(filename, 'w') as file:
+        file.write(book)
+    return filename
+
+
+def main():
+    for book_id in range(1, 11):
+        try:
+            url = f'https://tululu.org/b{book_id}/'
+            response = requests.get(url)
+            response.raise_for_status()
+            check_for_redirect(response)
+            soup = BeautifulSoup(response.text, 'lxml')
+            title, author = soup.find('h1').text.split('::')
+            download_txt(book_id, title.strip())
+        except HTTPError:
+            pass
+
+
+if __name__ == '__main__':
+    main()
